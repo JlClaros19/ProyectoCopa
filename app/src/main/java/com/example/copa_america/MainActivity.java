1c2c92b885eb976e6c7b637cf8d46ea38d5ccd5d
Jorge C <jorgeluisclarospary@gmail.com>
2019-06-06 16:07:07 -0400

    Commit

---
+++ app/src/main/java/com/example/copa_america/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.copa_america;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button btn1= (Button) findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String us= ((EditText)findViewById(R.id.usuario)).getText().toString();
+                String ct= ((EditText)findViewById(R.id.contraseña)).getText().toString();
+                if(us.equals("Admin") && ct.equals("1234"))
+                {
+                    Intent form = new Intent(MainActivity.this,PrincipalActivity.class);
+                    startActivity(form);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Datos Incorrectos",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
